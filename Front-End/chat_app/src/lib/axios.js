0434e741cae001341791dc7102527b7ad91c0e72
Danny2706 <163384051+Danny2706@users.noreply.github.com>
2025-09-13 20:15:15 +0300

    Update axios.js

--- Front-End/chat_app/src/lib/axios.js
+++ Front-End/chat_app/src/lib/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
     baseURL:
         import.meta.env.MODE === "development" ?
-            "http://localhost:5000/api": "/",
+            "https://ai-integrated-chat-app-5.onrender.com": "/",
     withCredentials: true
 })
