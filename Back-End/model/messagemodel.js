bfed934f0aa905fde5d0e149329294c7cf265ce7
Danny2706 <danikume27@gmail.com>
2026-08-03 18:25:59 +0300

    controller update

--- Back-End/model/messagemodel.js
+++ Back-End/model/messagemodel.js
@@ -1,31 +1,31 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const messageSchema = new mongoose.Schema(
-//   {
-//     senderId: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "User",
-//       required: true,
-//     },
-//     reciverId: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "User",
-//       required: true,
-//     },
-//     text: String,
-//     media: String,
+const messageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    reciverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: String,
+    media: String,
 
-//     type: {
-//       type: String,
-//       enum: ["text", "media", "ui"], 
-//       default: "text",
-//     },
-//     code: {
-//       type: String, 
-//     },
-//   },
-//   { timestamps: true }
-// );
+    type: {
+      type: String,
+      enum: ["text", "media", "ui"], 
+      default: "text",
+    },
+    code: {
+      type: String, 
+    },
+  },
+  { timestamps: true }
+);
 
-// const Message = mongoose.model("Message", messageSchema);
-// module.exports = Message;
+const Message = mongoose.model("Message", messageSchema);
+module.exports = Message;
