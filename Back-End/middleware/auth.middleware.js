71417e1cb5e2185ba28552d0adc6435db632bbe3
Danny2706 <danikume27@gmail.com>
2026-08-03 18:19:56 +0300

    adding middleware

--- Back-End/middleware/auth.middleware.js
+++ Back-End/middleware/auth.middleware.js
@@ -1,35 +1,35 @@
-const jwt = require("jsonwebtoken")
-const User = require("../model/usermodel")
-const catchAsyncError = require("../middleware/catchAsyncError")
+// const jwt = require("jsonwebtoken")
+// const User = require("../model/usermodel")
+// const catchAsyncError = require("../middleware/catchAsyncError")
 
- const protectRouter = catchAsyncError(async (req, res, next) => {
-    try {
-        const token = req.cookies.jwt
-        if (!token) {
-          return  res.status(401).json({ message: "token not found: Unauthorized." })
-        }
+//  const protectRouter = catchAsyncError(async (req, res, next) => {
+//     try {
+//         const token = req.cookies.jwt
+//         if (!token) {
+//           return  res.status(401).json({ message: "token not found: Unauthorized." })
+//         }
 
-        const decode = jwt.verify(token, process.env.JWT_SECRET)
+//         const decode = jwt.verify(token, process.env.JWT_SECRET)
 
-        if (!decode) {
-           return res.status(401).json({ message: "Invalid token: Unauthorized." })
-        }
+//         if (!decode) {
+//            return res.status(401).json({ message: "Invalid token: Unauthorized." })
+//         }
 
-        const user = await User.findById(decode.userId).select("-password")
+//         const user = await User.findById(decode.userId).select("-password")
 
-        if (!user) {
-            return res.status(404).json({ message: "user not found!" })
-        }
+//         if (!user) {
+//             return res.status(404).json({ message: "user not found!" })
+//         }
 
-        req.user = user
+//         req.user = user
     
-        next()
+//         next()
 
-    } catch (error) {
-        console.log("middleware error", error.message)
-        res.status(500).json({ message: "Server Error!" })
+//     } catch (error) {
+//         console.log("middleware error", error.message)
+//         res.status(500).json({ message: "Server Error!" })
     
-    }
-})
-module.exports = protectRouter
+//     }
+// })
+// module.exports = protectRouter
 
