99f4069b5d18ed8ca406e853ffd09838260d35b8
Danny2706 <danikume27@gmail.com>
2026-08-08 13:30:23 +0300

    socket.io fix

--- Back-End/lib/socket.js
+++ Back-End/lib/socket.js
@@ -1,41 +1,41 @@
-const { Server } = require("socket.io");
+// const { Server } = require("socket.io");
 
-const userSocketMap = {};
-let io;
+// const userSocketMap = {};
+// let io;
 
-function initSocket(server) {
-  io = new Server(server, {
-      cors: {
-    origin: [
-      "http://localhost:5173",
-      "https://ai-integrated-chat-app.vercel.app/"
-    ],
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-  });
+// function initSocket(server) {
+//   io = new Server(server, {
+//       cors: {
+//     origin: [
+//       "http://localhost:5173",
+//       "https://ai-integrated-chat-app.vercel.app/"
+//     ],
+//     methods: ["GET", "POST"],
+//     credentials: true,
+//   },
+//   });
 
-  io.on("connection", (socket) => {
-    const userId = socket.handshake.query.userId;
-     console.log("🔌 New client connected with userId:", userId);
-    if (userId) userSocketMap[userId] = socket.id;
-    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+//   io.on("connection", (socket) => {
+//     const userId = socket.handshake.query.userId;
+//      console.log("🔌 New client connected with userId:", userId);
+//     if (userId) userSocketMap[userId] = socket.id;
+//     io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-    socket.on("disconnect", () => {
-      delete userSocketMap[userId];
-      io.emit("getOnlineUsers", Object.keys(userSocketMap));
-    });
-  });
-}
+//     socket.on("disconnect", () => {
+//       delete userSocketMap[userId];
+//       io.emit("getOnlineUsers", Object.keys(userSocketMap));
+//     });
+//   });
+// }
 
-function getIo() {
-  if (!io) throw new Error("Socket.io not initialized yet!");
-  return io;
-}
+// function getIo() {
+//   if (!io) throw new Error("Socket.io not initialized yet!");
+//   return io;
+// }
 
-function getReceiversSocketId(userId) {
-  return userSocketMap[userId];
-}
+// function getReceiversSocketId(userId) {
+//   return userSocketMap[userId];
+// }
 
-module.exports = { initSocket, getReceiversSocketId, getIo };
+// module.exports = { initSocket, getReceiversSocketId, getIo };
 
